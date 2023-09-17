ab64d5f6c23e5405b761a91f552b54ea2da0e31a
Mariah A. Knowles <snotskie@gmail.com>
2023-09-17 10:42:21 -0500

    remove edit link

--- make.jl
+++ make.jl
@@ -5,6 +5,7 @@ using Documenter
 makedocs(
     sitename="Grounding AI",
     authors="Mariah A. Knowles <mariah.knowles@wisc.edu>",
+    remotes=nothing, # disable edit link
     format=Documenter.HTML(assets=[
         "assets/script.js",
         "assets/style.css"
